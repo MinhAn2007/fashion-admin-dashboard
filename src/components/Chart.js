af78b32e51763ac6248e7e9e30a76cc813d06fc0
an-innomizetech <an.vo@innomizetech.com>
2024-11-12 16:20:10 +0700

    fix height chart in dashboard

--- src/components/Chart.js
+++ src/components/Chart.js
@@ -97,7 +97,7 @@ const Dashboard = () => {
               ))}
             </select>
           </div>
-          <ResponsiveContainer width="100%" height={400}>
+          <ResponsiveContainer width="100%" height={550}>
             <LineChart data={lineData} margin={{ top: 5, right: 30, left: 20, bottom: 5 }}>
               <CartesianGrid strokeDasharray="3 3" />
               <XAxis dataKey="month" fontSize={12} />
